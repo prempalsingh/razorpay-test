a1e9dff701474d6b10d541558d30c6a270fdf035
Prempal Singh <prempal.42@gmail.com>
2016-03-23 19:29:23 +0530

    expense state dialog

--- app/src/main/java/com/prempal/expenses/ExpenseAdapter.java
+++ app/src/main/java/com/prempal/expenses/ExpenseAdapter.java
@@ -1,9 +1,13 @@
 package com.prempal.expenses;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by prempal on 23/3/16.
@@ -20,6 +24,14 @@ public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ViewHold
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
 
+        holder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(view.getContext());
+            }
+        });
+
+
     }
 
     @Override
@@ -29,9 +41,54 @@ public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ViewHold
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        TextView amount;
+        TextView description;
+        TextView time;
+        TextView state;
+        TextView category;
+        View view;
+
 
         public ViewHolder(View itemView) {
             super(itemView);
+            view = itemView;
+            category = (TextView) itemView.findViewById(R.id.tv_category);
+            description = (TextView) itemView.findViewById(R.id.tv_description);
+            amount = (TextView) itemView.findViewById(R.id.tv_amount);
+            category = (TextView) itemView.findViewById(R.id.tv_category);
+            time = (TextView) itemView.findViewById(R.id.tv_time);
+            state = (TextView) itemView.findViewById(R.id.tv_state);
         }
+    }
+
+    private void showDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Expense State");
+
+        String[] items = {"Verified", "Unverified", "Fraud"};
+        builder.setSingleChoiceItems(items, 0,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        builder.setPositiveButton("Ok",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        builder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
